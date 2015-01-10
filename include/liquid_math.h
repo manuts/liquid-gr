1faf6124f7e57de45dc7829571324d6f02f5f00c
Manu T S <manu.ts@outlook.com>
2015-01-10 16:43:14 +0100

    adding some math routines 	new file:   include/liquid_math.h 	new file:   src/math/liquid_math.cc   --

---
+++ include/liquid_math.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2014 Manu T S
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIQUID_MATH_H
+#define LIQUID_MATH_H
+
+#include <math.h>
+#include <complex>
+#include <liquid/liquid.h>
+
+namespace liquid {
+  namespace math {
+    const std::complex<float> I(0.0, 1.0);
+    std::complex<float> cexpjf(float theta);
+  }
+}
+
+#endif
---
+++ src/math/liquid_math.cc
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014 Manu T S
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "liquid_math.h"
+
+namespace liquid {
+  namespace math {
+    std::complex<float> cexpjf(float theta) {
+      return cosf(theta) + I*sinf(theta);
+    }
+  }
+}
